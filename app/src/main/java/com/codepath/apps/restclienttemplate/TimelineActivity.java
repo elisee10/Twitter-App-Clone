2750e796ab0d0936075a047c67358ff40b0493fa
Elisee Djapa <eliseedjapa@gmail.com>
2020-10-12 17:31:22 -0400

    MVP all done

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -3,6 +3,7 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +27,7 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter adapter;
+    SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,19 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
         client = TwitterApp.getRestClient(this);
+        swipeContainer = findViewById(R.id.swipeContainer);
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                populateHomeTimeline();
+
+
+            }
+        });
 
         //Find the recycler views
         rvTweets = findViewById(R.id.rvTweets);
@@ -42,7 +57,6 @@ public class TimelineActivity extends AppCompatActivity {
         //configure the recycler view: layout manager and adapter
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(adapter);
-
         populateHomeTimeline();
 
     }
@@ -55,8 +69,10 @@ public class TimelineActivity extends AppCompatActivity {
                 JSONArray jsonArray = json.jsonArray;
                 //List<Tweet> tweets =  Tweet.fromJsonArray(jsonArray);
                 try {
-                     tweets.addAll(Tweet.fromJsonArray(jsonArray));
-                     adapter.notifyDataSetChanged();
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    swipeContainer.setRefreshing(false);
+                   
                    // Tweet.fromJsonArray(jsonArray);
                 } catch (JSONException e) {
                     e.printStackTrace();
--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -45,8 +45,19 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     }
 
     @Override
-    public int getItemCount() {
-        return tweets.size();
+    public int getItemCount() { return tweets.size(); }
+
+    //Clean all the elements of the recycler
+    public void clear(){
+
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+    //Add a list
+    public void addAll(List<Tweet> tweetList){
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+
     }
 
     //pass in the context and list of tweets
